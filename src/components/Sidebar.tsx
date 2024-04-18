b81f5a8d4f6ea45b2d7b0dc43a94b2d75f8edc71
Hazel Belmont <hparkerbelmont@gmail.com>
2024-04-18 08:06:05 -0500

    use 3 columns (temporary for worlds, will make more dynamic later)

--- src/components/Sidebar.tsx
+++ src/components/Sidebar.tsx
@@ -34,7 +34,7 @@ function StatusPanel({teamKey, status}: {teamKey: string, status: object}) {
 }
 
 function RankList({allStatuses}: {allStatuses: object}) {
-    let column: JSX.Element[][] = [[], []];
+    let column: JSX.Element[][] = [[], [], []];
     let statusArray: object[] = [];
     Object.keys(allStatuses).forEach((key) => {
         allStatuses[key]["teamKey"] = key
@@ -44,11 +44,14 @@ function RankList({allStatuses}: {allStatuses: object}) {
         return teamObjectRankLookup(a) - teamObjectRankLookup(b);
     })
     for (let i = 0; i < statusArray.length; i++) {
-        if(i === 0 || i < statusArray.length / 2) {
-            column[0].push(<p className='rankings'>Rank {statusArray[i]["qual"]["ranking"]["rank"]}: {(statusArray[i]["teamKey"] as string).replace("frc", "")}</p>);
+        if(i === 0 || i < statusArray.length / 3) {
+            column[0].push(<p className='rankings'>Rank {teamObjectRankLookup(statusArray[i])}: {(statusArray[i]["teamKey"] as string).replace("frc", "")}</p>);
+        }
+        else if(i >= statusArray.length / 3 && i < (2 * statusArray.length) / 3){
+            column[1].push(<p className='rankings'>Rank {teamObjectRankLookup(statusArray[i])}: {(statusArray[i]["teamKey"] as string).replace("frc", "")}</p>);
         }
         else {
-            column[1].push(<p className='rankings'>Rank {statusArray[i]["qual"]["ranking"]["rank"]}: {(statusArray[i]["teamKey"] as string).replace("frc", "")}</p>);
+            column[2].push(<p className='rankings'>Rank {teamObjectRankLookup(statusArray[i])}: {(statusArray[i]["teamKey"] as string).replace("frc", "")}</p>);
         }
     }
 
@@ -56,6 +59,7 @@ function RankList({allStatuses}: {allStatuses: object}) {
         <div id="matches">
             <div className="column">{column[0]}</div>
             <div className="column">{column[1]}</div>
+            <div className="column">{column[2]}</div>
         </div>
     )
 }
